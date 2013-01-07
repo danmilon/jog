86dc3a1288d6756a52c1ee21e095a02c93a82016
Dan Milon <danmilon@gmail.com>
2013-01-07 20:19:20 +0200

    respect log level

--- test/jog.js
+++ test/jog.js
@@ -46,26 +46,51 @@ describe('Jog', function(){
       log.error('even worse', err);
       done();
     })
-  })
 
-  describe('#ns(obj)', function(){
-    it('should return a Jog', function(done){
+    it('should respect the log level', function (done) {
+      var times = 0;
       var store = {
-        add: function(obj){
-          obj.vid.should.equal('abc');
-          obj.uid.should.equal('tobi');
-          obj.level.should.equal('info');
-          obj.timestamp.should.be.a('number');
-          obj.type.should.equal('something happened');
-          done();
+        add: function (obj) {
+          times++;
+          obj.message.should.be.ok;
+          if (times === 2) {
+            done();
+          }
         }
-      };
+      }
 
-      var log = new Jog(store);
-      var orig = log;
-      log = log.ns({ vid: 'abc' }).ns({ uid: 'tobi' });
-      log.should.not.equal(orig);
-      log.info('something happened');
+      var log = new Jog(store, {}, { level: 'info' });
+      log.debug('this', { message: false });
+      log.warn('this', { message: true });
+
+      log.level = 'debug';
+
+      log.debug('this', { message: true });
+
+      log.level = 'error';
+
+      log.warn('this', { message: false });
+    })
+
+    describe('#ns(obj)', function(){
+      it('should return a Jog', function(done){
+        var store = {
+          add: function(obj){
+            obj.vid.should.equal('abc');
+            obj.uid.should.equal('tobi');
+            obj.level.should.equal('info');
+            obj.timestamp.should.be.a('number');
+            obj.type.should.equal('something happened');
+            done();
+          }
+        };
+
+        var log = new Jog(store);
+        var orig = log;
+        log = log.ns({ vid: 'abc' }).ns({ uid: 'tobi' });
+        log.should.not.equal(orig);
+        log.info('something happened');
+      })
     })
   })
 })
